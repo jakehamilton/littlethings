563744a493aeffd75e5908bac03ff3f62e4de8dd
Lily Scott <git@suchipi.tv>
2022-02-16 03:21:17 -0700

    feat: more testing wip

--- packages/test/src/index.ts
+++ packages/test/src/index.ts
@@ -1,102 +1,203 @@
 type TestAPI = {
-	describe: (description: string, init: () => void | Promise<void>) => void;
-	it: (description: string, init: () => void | Promise<void>) => void;
-	beforeEach: (init: () => void | Promise<void>) => void;
-	beforeAll: (init: () => void | Promise<void>) => void;
-	afterEach: (init: () => void | Promise<void>) => void;
-	afterAll: (init: () => void | Promise<void>) => void;
+	describe: (
+		description: string,
+		body: (api: TestAPI) => void | Promise<void>
+	) => void;
+	it: (description: string, body: () => void | Promise<void>) => void;
+	beforeEach: (body: () => void | Promise<void>) => void;
+	beforeAll: (body: () => void | Promise<void>) => void;
+	afterEach: (body: () => void | Promise<void>) => void;
+	afterAll: (body: () => void | Promise<void>) => void;
 };
 
 type RunMode = "NORMAL" | "IGNORED" | "FOCUSED";
 
 class Test {
+	type = "Test" as const;
+
 	context: Array<string>;
-	callback: () => void | Promise<void>;
+	body: () => void | Promise<void>;
 	runMode: RunMode;
+	parent: Describe | null = null;
 
 	constructor({
 		context,
 		runMode = "NORMAL",
-		callback,
+		body,
 	}: {
 		context: Array<string>;
 		runMode?: RunMode;
-		callback: () => void | Promise<void>;
+		body: () => void | Promise<void>;
 	}) {
 		this.context = context;
 		this.runMode = runMode;
-		this.callback = callback;
+		this.body = body;
 	}
 }
 
 class Describe {
+	type = "Describe" as const;
+
 	context: Array<string>;
 	runMode: RunMode;
-	init: () => void | Promise<void>;
+	body: (api: TestAPI) => void | Promise<void>;
+	parent: Describe | null = null;
 
 	rootDescribe: Describe;
 
 	children: Array<Test | Describe> = [];
-	befores: Array<LifecycleEvent> = [];
-	afters: Array<LifecycleEvent> = [];
+	befores: Array<LifecycleHook> = [];
+	afters: Array<LifecycleHook> = [];
+
+	api: TestAPI;
+
+	hasRunBody: boolean = false;
 
 	constructor({
 		context,
 		runMode = "NORMAL",
-		init,
+		body,
 		rootDescribe,
 	}: {
 		context: Array<string>;
 		runMode?: RunMode;
-		init: () => void | Promise<void>;
-		rootDescribe: Describe;
+		body: (api: TestAPI) => void | Promise<void>;
+		rootDescribe?: Describe;
 	}) {
 		this.context = context;
 		this.runMode = runMode;
-		this.init = init;
-		this.rootDescribe = rootDescribe;
-	}
+		this.body = body;
+		this.rootDescribe = rootDescribe || this;
 
-	setup() {}
-
-	getAPI(): TestAPI {
-		return {
-			describe: (description, init) => {
+		this.api = {
+			describe: (description, body) => {
 				const child = new Describe({
 					context: this.context.concat(description),
-					init,
+					body,
 					rootDescribe: this.rootDescribe,
 				});
+				child.parent = this;
 				this.children.push(child);
 			},
-			it: (description, callback) => {
+			it: (description, body) => {
 				const child = new Test({
 					context: this.context.concat(description),
-					callback,
+					body,
 				});
+				child.parent = this;
 				this.children.push(child);
 			},
-			beforeEach: (callback) => {
-				this.befores.push({ callback });
+			beforeEach: (body) => {
+				this.befores.push(
+					new LifecycleHook({
+						context: this.context.concat("beforeEach"),
+						body,
+					})
+				);
 			},
-			beforeAll: (callback) => {
-				this.rootDescribe.befores.push({ callback });
+			beforeAll: (body) => {
+				this.rootDescribe.befores.push(
+					new LifecycleHook({
+						context: this.context.concat("beforeAll"),
+						body,
+					})
+				);
 			},
-			afterEach: (callback) => {
-				this.afters.push({ callback });
+			afterEach: (body) => {
+				this.afters.push(
+					new LifecycleHook({
+						context: this.context.concat("afterEach"),
+						body,
+					})
+				);
 			},
-			afterAll: (callback) => {
-				this.rootDescribe.afters.push({ callback });
+			afterAll: (body) => {
+				this.rootDescribe.afters.push(
+					new LifecycleHook({
+						context: this.context.concat("afterAll"),
+						body,
+					})
+				);
 			},
 		};
 	}
+
+	runBody() {
+		if (this.hasRunBody) return;
+
+		this.body(this.api);
+		for (const child of this.children) {
+			if (child.type === "Describe") {
+				child.runBody();
+			}
+		}
+	}
 }
 
-type LifecycleEvent = {
-	callback: () => void | Promise<void>;
-};
+class LifecycleHook {
+	type = "LifecycleHook" as const;
+
+	context: Array<string>;
+	body: () => void | Promise<void>;
+	parent: Describe | null = null;
+
+	constructor({
+		context,
+		body,
+	}: {
+		context: Array<string>;
+		body: () => void | Promise<void>;
+	}) {
+		this.context = context;
+		this.body = body;
+	}
+}
+
+type TestEvent =
+	| {
+			type: "started";
+			subject: Test | Describe | LifecycleHook;
+	  }
+	| {
+			type: "result";
+			subject: Test | Describe | LifecycleHook;
+			status: "PASSED" | "FAILED" | "SKIPPED" | "ERRORED";
+	  };
 
 class TestSuite {
-	// currentAPI: TestAPI;
-	children: Array<Test | Describe> = [];
+	rootDescribe: Describe;
+	api: TestAPI;
+
+	constructor() {
+		this.rootDescribe = new Describe({
+			context: [],
+			body: () => {},
+		});
+		this.api = this.rootDescribe.api;
+	}
+
+	run({
+		onResult = () => {},
+	}: {
+		onResult?: (result: TestResult) => void;
+	}): Promise<Array<TestResult>> {
+		this.rootDescribe.runBody();
+
+		const results: Array<TestResult> = [];
+		const workToDo: Array<() => void | Promise<void>> = [];
+
+		function prepareWork(describe: Describe) {
+			for (const child of describe.children) {
+				if (child.type === "Test") {
+					workToDo.push(async () => {
+						for (const before of describe.befores) {
+							workToDo.push(() => before.body());
+						}
+					});
+				}
+			}
+		}
+
+		return results;
+	}
 }
